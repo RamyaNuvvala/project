2741207efab75174db5d1b1b1e60460d9a13e591
RamyaNuvvala <119427580+RamyaNuvvala@users.noreply.github.com>
2023-11-22 22:46:40 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,20 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<
-        }
+import streamlit as st
 
-        /* Add more styles as needed */
-    </style>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=de
+def main():
+    st.title("College Information Website with Chatbot")
+    st.markdown("# Welcome to Our College Information Website!")
 
-    <!-- Add your content here -->
+    # Add your content here
+
+    # Dialogflow Messenger
+    st.markdown("## College Chatbot")
+    st.markdown("""
     <iframe
-    allow="microphone;"
-    width="350"
-    height="430"
-    src="https://console.dialogflow.com/api-client/demo/embedded/84ec9eb3-eb15-4185-81d9-317b1a5f3d70">
-</iframe>
+        allow="microphone;"
+        width="350"
+        height="430"
+        src="https://console.dialogflow.com/api-client/demo/embedded/84ec9eb3-eb15-4185-81d9-317b1a5f3d70">
+    </iframe>
+    """, unsafe_allow_html=True)
 
-</body>
-</html>
+if __name__ == "__main__":
+    main()
+
